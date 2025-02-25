02895acf7a751017d00f3f56bc0f0a65719b9d55
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-02-24 22:32:07 -0700

    Got input from the user and split it into an array of words

--- Program.cs
+++ Program.cs
@@ -4,3 +4,9 @@ Console.Clear();
 
 /*Creating the greeting*/
 Console.WriteLine("Let's manipulate your phrase into pig latin and encode it!");
+
+/*Getting message from user*/
+Console.Write("Please enter your message: ");
+
+string userPhrase = Console.ReadLine();
+string[] words = userPhrase.Split(" "); //splits the user's phrase into an array of words
